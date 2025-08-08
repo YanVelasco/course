c8fff61a45e542170483c811bdc844217edfb123
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-08-08 17:42:35 -0300

    add CourseModel, CourseLevel, and CourseStatus enums for course management

---
+++ src/main/java/com/ead/course/models/CourseModel.java
@@ -0,0 +1,63 @@
+package com.ead.course.models;
+
+import com.ead.course.enums.CourseLevel;
+import com.ead.course.enums.CourseStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_COURSES")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CourseModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "course_id", nullable = false, unique = true)
+    private String courseId;
+
+    @Column(name = "name", nullable = false, unique = true, length = 150)
+    private String name;
+
+    @Column(name = "description", nullable = false, length = 2000)
+    private String description;
+
+    @Column(name = "creation_date", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime creationDate;
+
+    @Column(name = "last_update_date", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime lastUpdateDate;
+
+    @Column(name = "course_status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CourseStatus courseStatus;
+
+    @Column(name = "course_level", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CourseLevel courseLevel;
+
+    @Column(name = "user_instructor", nullable = false)
+    private UUID userInstructor;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+}
